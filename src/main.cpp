6c2be5627b46218e24fbf317aa7bfe9f7b089015
Anders Syvertsen <andesyv@gmail.com>
2020-11-10 14:28:39 +0100

    Basic command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cxxopts.hpp>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+int main(int argc, char *argv[]) {
+    cxxopts::Options options("GitPRComp", "Git comparison tool for commit ranges");
+
+    options.add_options()
+        ("f,format", "Output format", cxxopts::value<std::string>()->default_value("html"))
+        ("h,help", "Print usage");
+
+    const auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+      std::cout << options.help() << std::endl;
+      return 0;
+    }
+
+    auto format = result["format"].as<std::string>();
+
+    
+
     return 0;
 }
